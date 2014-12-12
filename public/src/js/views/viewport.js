ce7d6a4d31caca18215466429509e0d2a62a0af2
Scott Skow <scott38611@comcast.net>
2014-12-11 21:58:38 -0700

    fixing footer styling. commenting viewport

--- public/src/js/views/viewport.js
+++ public/src/js/views/viewport.js
@@ -1,21 +1,42 @@
 var BaseView = require('./base');
 var _ = require('underscore');
 
+/**
+ * Class ViewPort
+ *
+ * This is the main view for the entire application. It will render
+ * all the partials onto the document to formulate the view across
+ * all page renders. This includes the header, footer, and whatever
+ * else makes up the application view.
+ *
+ */
 module.exports = BaseView.extend({
 
+  //render a partial template to document.body
+  _renderPartial: function(templateId, data) {
+    var tpl= this.$html(templateId); //retrieve html template from DOM
+    var template = _.template(tpl); //convert to template func
+    this.$('body').append(template(data)); //append tpl to document
+    return this;
+  },
+
+  //render the header as a partial
   _renderHeader: function() {
     var user = this.getUser();
-    var tpl= this.$html('#tpl-header');
-    var template = _.template(tpl);
-    this.$('body').append(template({username: user.name}));
+    this._renderPartial('#tpl-header', {
+      username: user.name
+    });
+
+    return this;
   },
 
+  //render the footer as a partial
   _renderFooter: function() {
-    var tpl= this.$html('#tpl-footer');
-    var template = _.template(tpl);
-    this.$('body').append(template());
+    this._renderPartial('#tpl-footer');
+    return this;
   },
 
+  //render viewport
   render: function() {
     console.log('rendering viewport');
     this._renderHeader();
