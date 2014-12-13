dfcb129c26d83195e48b45ac518592765a001a69
Scott Skow <scott38611@comcast.net>
2014-12-12 17:54:39 -0700

    adding global initializer

---
+++ app/routes/app.js
@@ -0,0 +1,25 @@
+var auth = require('../auth');
+
+/* GET home page. */
+function index(req, res) {
+  res.render('index', { title: 'Home'});
+}
+
+//GET: render login view
+function login(req, res) {
+  res.render('login');
+}
+
+//GET: log a user out
+function logout(req, res) {
+  delete req.user;
+  delete req.session.user;
+  res.redirect('/');
+}
+
+module.exports = {
+  index: index,
+  login: login,
+  authenticate: auth.authenticate,
+  logout: logout
+};
