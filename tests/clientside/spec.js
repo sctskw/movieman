ec503731581465d472bdeb937c646cabba1b3701
Scott Skow <scott38611@comcast.net>
2014-12-15 02:12:34 -0700

    renaming described tests

--- tests/clientside/spec.js
+++ tests/clientside/spec.js
@@ -1,8 +1,8 @@
 var Model = require('./models/movie');
 
 
-describe('some test', function() {
-  it('should pass', function() {
+describe('MovieModel', function() {
+  it('should have urlRoot /api/movies/', function() {
     var movie = new Model();
     expect(movie.urlRoot).to.equal('/api/movies/');
   })
